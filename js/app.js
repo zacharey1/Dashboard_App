66dc5cf49a543ed42ba236b40c94d7cb496648c7
zacharey1 <konohaha77@gmail.com>
2022-11-01 14:17:00 +0900

    js update

--- js/app.js
+++ js/app.js
@@ -126,3 +126,19 @@ let mobileChart = new Chart(mobileCanvas, {
 });
 
 //messaging section
+
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
+
+send.addEventListener('click', () => {
+    if (user.value === "" && message.value === "") {
+        alert("Please fill out user and message fields before sending");
+    } else if (user.value === "" ) {
+        alert("Please fill out user field before sending");
+    } else if (message.value === "" ) {
+        alert("Please fill out message field before sending");
+    } else {
+        alert(`Message successfully sent to: ${user.value}`);
+    }
+});
